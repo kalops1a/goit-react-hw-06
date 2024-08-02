5b7f268dadc79fb38705e89016bba03ee975a22b
kalops1a <vadymduchenko@gmail.com>
2024-08-02 15:23:29 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Searchbox from './components/Searchbox';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Searchbox from './Searchbox';
 
 const App = () => (
   <div>
     <h1>Phonebook</h1>
     <ContactForm />
+    <h2>Contacts</h2>
     <Searchbox />
     <ContactList />
   </div>
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,39 +1,21 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts, deleteContact } from '../redux/contactsSlice';
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../redux/contactsSlice';
 import { selectNameFilter } from '../redux/filtersSlice';
+import Contact from './Contact';
 
 const ContactList = () => {
-  const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
 
-  if (!Array.isArray(contacts)) {
-    console.error('Contacts should be an array');
-    return null;
-  }
-
-  if (typeof filter !== 'string') {
-    console.error('Filter should be a string');
-    return null;
-  }
-
-  const filteredContacts = contacts.filter(contact => {
-    if (typeof contact.name === 'string') {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
-    } else {
-      console.error(`Expected contact.name to be a string, but got ${typeof contact.name}`);
-      return false;
-    }
-  });
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul>
       {filteredContacts.map(contact => (
-        <li key={contact.id}>
-          {contact.name}: {contact.number}
-          <button onClick={() => dispatch(deleteContact(contact.id))}>Delete</button>
-        </li>
+        <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
   );
